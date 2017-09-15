cdfc2e7fabd333112b3e6b42c0f8265b1d1c3f74
JEmilioC <28cllje@gmail.com>
2017-09-15 16:19:50 -0500

    2

--- TESTThree.py
+++ TESTThree.py
@@ -3,3 +3,10 @@ import pyperclip
 a=pyperclip.paste()
 
 print(a)
+
+dicti={'prueba1':'Emilio','prueba2':'emilio'}
+
+for rof in dicti:
+    print (rof)
+
+print (dicti['prueba1']+' and '+dicti['prueba2'])
--- jsontry.py
+++ jsontry.py
@@ -3,4 +3,10 @@ import json
 with open('ej.json') as jsonEscuela:
     data=json.load(jsonEscuela)
 
-print(data['alumnos'])
+print(data['Escuela'])
+
+data_string=json.dumps(data)
+print (data_string)
+decoded= json.loads(data_string)
+print (decoded)
+# print (str(decoded["Escuela"[1]["Verduras"][0]["Cantidad"]+"Alumnos."]))
